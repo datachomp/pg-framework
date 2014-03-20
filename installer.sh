4fbef7755cd1cd8656693b107d655a423a70c00c
rob sullivan <rob@datachomp.com>
2014-03-19 20:40:40 -0500

    v1 of the installer

---
+++ installer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Get the basics"
+
+sudo apt-get -y install software-properties-common python-software-properties
+sudo apt-get -y install curl git-core
+sudo apt-get -y install p7zip-rar p7zip-full
+
+sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install pgdg-keyring
+
+sudo apt-get -y install memcached
+sudo apt-get -y install redis-server
+sudo apt-get -y install postgresql-9.3
+sudo apt-get -y install postgresql-9.3-contrib
+sudo apt-get -y install postgresql-9.3-plv8
+
+
+echo "horray!"
+exit 0
